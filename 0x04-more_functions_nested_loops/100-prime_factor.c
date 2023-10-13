4f27e44bd7fe2025fc306509245c2b39bf657d79
Nehal333 <nihalabdo333@gmail.com>
2023-10-13 11:55:13 -0700

    updated

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,15 +6,15 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
         long int n;
         long int max;
         long int i;
+
         n = 612852475143;
         max = -1;
+
         while (n % 2 == 0)
         {
                 max = 2;
