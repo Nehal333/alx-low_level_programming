bbf229db5069c031d6d30862232da1619578f6da
Nehal333 <nihalabdo333@gmail.com>
2023-11-08 13:47:16 -0800

    upp

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -21,7 +21,7 @@ int op_add(int a, int b)
  * op_sub - Returns the difference of two numbers.
  * @a: The first number.
  * @b: The second number
- * 
+ *
  * Return: The difference of a and b
  */
 int op_sub(int a, int b)
@@ -39,7 +39,7 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
-/*
+/**
  * op_div - Returns the division of two numbers
  * @a: The first number
  * @b: The second number
